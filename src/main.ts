2a78588a9a7762ae184a9431fc309e126fd9fa8e
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-06-09 18:46:58 +0200

    export helpers

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,4 @@ export * from "./functions/types";
 export * from "./functions/operators";
 export * from "./functions/query-helper";
 export * from "./functions/operators";
+export * from "./functions/helpers";
